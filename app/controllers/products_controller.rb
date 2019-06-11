9527290c928fc82844faea025c75df37deae2f0f
Yosuke <s7k315@gmail.com>
2019-06-11 22:10:20 +0900

    ajax

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,12 +11,23 @@ class ProductsController < RankingController
     @product = Product.find(params[:id])
   end
 
+  # SQLインジェクションリスクあり
+  # def search
+  #   @products = Product.find_by_sql("select * from products where title like '%#{params[:keyword]}%' LIMIT 20")
+  # end
+
+  # SQLインジェクション対策済み
   def search
-    # 検索フォームのキーワードをあいまい検索して、productsテーブルから20件の作品情報を取得する
-    @products = Product.where('title LIKE(?)', "%#{params[:keyword]}%").limit(20)
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    keyword = "%#{params[:keyword]}%"
+    @products = Product.find_by_sql(["select * from products where title like ? LIMIT 20", keyword])
   end
+
+  # def search
+  #   # 検索フォームのキーワードをあいまい検索して、productsテーブルから20件の作品情報を取得する
+  #   @products = Product.where('title LIKE(?)', "%#{params[:keyword]}%").limit(20)
+  #   respond_to do |format|
+  #     format.html
+  #     format.json
+  #   end
+  # end
 end
